935a832d2cbb9576e1f310ea6d0ca308b3ef6680
Mateus Melo <mateus@mateusmelo.com>
2026-06-29 11:52:18 -0300

    Update program.cs to allow cors

--- OutEHR/src/OutEHR.Api/Program.cs
+++ OutEHR/src/OutEHR.Api/Program.cs
@@ -10,10 +10,22 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
